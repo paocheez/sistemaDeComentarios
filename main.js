d7aa8d5e8069ededde12288e6ab1e5dbcaaf2f84
Paola Conde <pao.cheez@gmail.com>
2022-02-23 12:06:06 -0600

    add comments container sructure

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const comments = []
 
-const inputContainer = document.createElement("div")
-const input = document. createElement("input")
+const inputContainer = document.createElement('div')
+const input = document. createElement('input')
+const commentsContainer = document.querySelector('#comments-container')
+
+input.classList.add('input')
+
+input.addEventListener('keydown', e=> {
+
+})
+
+commentsContainer.appendChild(inputContainer)
+inputContainer.appendChild(input)
